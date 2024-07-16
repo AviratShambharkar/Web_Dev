20685da82356b92391292a72e60566d4ed8a0b42
AviratShambharkar <128232370+AviratShambharkar@users.noreply.github.com>
2024-07-16 23:59:50 +0530

    Update App.jsx

--- FrontEnd/online_judge/src/App.jsx
+++ FrontEnd/online_judge/src/App.jsx
@@ -3,35 +3,41 @@ import {
   Route,
   Routes,
   Navigate,
+  useLocation,
 } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import SignUpPage from "./components/SignUpPage";
 import HomePage from "./components/HomePage";
 import ForgotPasswordPage from "./components/ForgotPasswordPage";
 import ResetPasswordPage from "./components/ResetPasswordPage";
-import Profile from "./components/Profile"; // Import the Profile component
-// import Navbar from "./components/Navbar"; // Uncomment this if you want to use Navbar
+import Profile from "./components/Profile";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <div>
-        {/* Uncomment and use Navbar if needed */}
-        {/* <Navbar /> */}
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signUp" element={<SignUpPage />} />
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route
-            path="/reset-password/:token"
-            element={<ResetPasswordPage />}
-          />
-          <Route path="/profile" element={<Profile />} /> {/* Add this line */}
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+  const hideNavbarPaths = ["/login", "/signUp"];
+
+  return (
+    <div>
+      {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signUp" element={<SignUpPage />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </div>
   );
 }
 
