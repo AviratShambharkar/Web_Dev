bc68c9e2d988e068e0b5166f989e004f6a93b414
AviratShambharkar <128232370+AviratShambharkar@users.noreply.github.com>
2024-07-27 18:51:13 +0530

    Create codeService.js

---
+++ FrontEnd/online_judge/src/services/codeService.js
@@ -0,0 +1,40 @@
+import axiosInstance from "./axiosInstance";
+
+export const saveUserCode = async (problemId, userId, code, language) => {
+  try {
+    const response = await axiosInstance.post("/user/saveUserCode", {
+      problemId,
+      userId,
+      code,
+      language,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error saving user code:", error);
+    throw error;
+  }
+};
+
+export const getUserCode = async (problemId, userId, language) => {
+  try {
+    const response = await axiosInstance.get("/user/getUserCode", {
+      params: { problemId, userId, language },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user code:", error);
+    throw error;
+  }
+};
+
+export const getAllSubmissions = async (problemId) => {
+  try {
+    const response = await axiosInstance.get(
+      `submissions/submissions/${problemId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching submissions:", error);
+    throw error;
+  }
+};
